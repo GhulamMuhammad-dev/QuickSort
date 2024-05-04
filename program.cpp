52ba4825edc3e7a757d36a204a9c9a17530be697
GhulamMuhammad-dev <ghulamMuhammad.dev@gmail.com>
2024-05-04 23:05:56 +0500

    The second commit of today

--- program.cpp
+++ program.cpp
@@ -1,6 +1,61 @@
 #include<iostream>
 using namespace std;
 
+
+
+ int partition(int *array,int l,int h){
+         int paviot=array[l];
+         int i=l;
+         int j=h;
+
+
+         while(i<j){
+         do{
+            i++;
+         }while(array[i]>=paviot);
+
+
+         do{
+            j--;
+         }while(array[j]<=paviot);
+         
+         if(i<j){
+            int temp=array[i];
+            array[i]=array[j];
+            array[j]=temp;
+         }
+         
+         }
+    int temp=array[l];
+            array[l]=array[j];
+            array[j]=temp;
+
+
+            return j;
+
+        
+    }
+
+
 int main(){
+
+const int size=7;
+int i=0;
+int j=size-1;
+
+int randomArray[size]={5,3,6,2,8,1,7};
+
+cout<<partition(randomArray,i,j);
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
